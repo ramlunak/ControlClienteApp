d7130b59c65d76d0ca356346a2e63d54b12db351
Royber <royber@yardim.com.br>
2021-02-27 15:28:40 -0300

    home page con grafico

--- ControlClienteApp/ControlClienteApp/views/HomePage.xaml.cs
+++ ControlClienteApp/ControlClienteApp/views/HomePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microcharts;
+using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +16,50 @@ namespace ControlClienteApp.views
     {
         public HomePage()
         {
-            InitializeComponent();           
+            InitializeComponent();
+            LoadChart();
         }
+
+        public void LoadChart()
+        {
+            var entries = new[]
+ {
+     new ChartEntry(212)
+     {
+         Label = "Finalizadas",
+         ValueLabel = "25",
+         TextColor = SKColor.Parse("#27AE60"),        
+         ValueLabelColor = SKColor.Parse("#27AE60"),
+         Color = SKColor.Parse("#27AE60")
+     },
+     new ChartEntry(248)
+     {
+         Label = "Canceladas",
+         ValueLabel = "3",
+        TextColor = SKColor.Parse("#E74C3C"),
+         ValueLabelColor = SKColor.Parse("#E74C3C"),
+         Color = SKColor.Parse("#E74C3C")
+     },
+     new ChartEntry(128)
+     {
+         Label = "Pendientes",
+         ValueLabel = "10",
+         TextColor = SKColor.Parse("#F5B041"),
+         ValueLabelColor = SKColor.Parse("#F5B041"),
+         Color = SKColor.Parse("#F5B041")
+     }
+     };
+
+            var chart = new DonutChart()
+            {
+                Entries = entries,
+                AnimationDuration = TimeSpan.FromSeconds(3),
+                LabelTextSize = 20
+
+            };
+            this.chartView.Chart = chart;
+
+        }
+
     }
 }
